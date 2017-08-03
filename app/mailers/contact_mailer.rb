e73db44eada52889d3486220c1d12fbc4930b800
Dashiell Guillermo <dashielldash@protonmail.com>
2017-08-03 21:11:38 +0000

    Final commit

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,12 +1,9 @@
-class ContactMailer < ActionMailer::
-    default to: 'dashielldash@protonmail.com'
-    
-    def contact_email(name, email, body)
+class ContactMailer < ActionMailer::Base
+  default to: 'emildguillermo@gmail.com'
+  def contact_email(name, email, body)
     @name = name
     @email = email
     @body = body
-    
     mail(from: email, subject: 'Contact Form Message')
-    end
-    
+  end
 end
