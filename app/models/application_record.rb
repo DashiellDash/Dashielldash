23e21aa66bd2c3f1c250b5a72ed15f1c74f16d6b
Dashiell Guillermo <dashielldash@protonmail.com>
2017-08-06 01:39:47 +0000

    finally fixed devise gem

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,2 +1,2 @@
-class Contact < ActiveRecord::Base
+class ApplicationRecord < ActiveRecord::Base
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170805235845) do
+ActiveRecord::Schema.define(version: 20170806011838) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
